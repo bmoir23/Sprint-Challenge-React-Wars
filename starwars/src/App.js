f664188c967f28bd91471e5ecebb8d4d1924c7f2
Brian Moir <brianmoir@Brians-MacBook-Pro.local>
2019-09-29 21:52:01 -0400

    hmm

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -21,13 +21,11 @@ const App = () => {
         if (!chars) return <h3 > Loading... < /h3>
         return ( <
             div className = "App" >
-
-
             <
             h1 className = "Header" > React Wars < /h1> {
             chars.map((char, key) => < CharCard char = { char }
                 key = { key }
-                /> )}   <
+                /> )}    <
                 /div>
             );
         }
