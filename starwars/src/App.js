cb1a4f692d7714557d8efc2fcceba2efb55ab58c
Brian Moir <brianmoir@Brians-MacBook-Pro.local>
2019-09-29 21:37:57 -0400

    Vs code went crazy

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -7,10 +7,10 @@ import CharCard from './components/CharCard/CharCard';
 const App = () => {
         // Try to think through what state you'll need for this app before starting. Then build out
         // the state properties here.
-        const [chars, setChars] = useState()
-            // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-            // side effect in a component, you want to think about which state and/or props it should
-            // sync up with, if any.
+        const [chars, setChars] = useState({});
+        // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+        // side effect in a component, you want to think about which state and/or props it should
+        // sync up with, if any.
         useEffect(() => {
             axios.get('https://swapi.co/api/people')
                 .then(res => {
@@ -20,14 +20,15 @@ const App = () => {
         }, [])
         if (!chars) return <h3 > Loading... < /h3>
         return ( <
-                div className = "App" >
-                <
-                h1 className = "Header" > React Wars < /h1> {
-                    chars.map((char, key) => < CharCard char = { char }
-                        key = { key }
-                        /> )} <
-                        /div>
-                    );
-                }
+            div className = "App" >
 
-                export default App;
+            <
+            h1 className = "Header" > React Wars < /h1> {
+            chars.map((char, key) => < CharCard char = { char }
+                key = { key }
+                /> )}  <
+                /div>
+            );
+        }
+
+        export default App;
