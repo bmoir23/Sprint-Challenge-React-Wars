dabcbd849ed801853676231b01cbb6db9e8a5dbb
Brian Moir <brianmoir@Brians-MacBook-Pro.local>
2019-09-29 21:32:45 -0400

    commit

---
+++ starwars/src/components/CharCard/CharCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import CharInfo from '../CharInfo/CharInfo';
+
+
+const Card = styled.div `
+    width: 500px;
+    margin: 10px;
+    display: flex;
+    border: 2px solid black;
+    `;
+
+const Name = styled.h3 `
+    text-align: center;
+    width: 40%;
+    align-self: center;
+    `;
+
+const CharCard = (props) => {
+    return ( <
+        Card >
+        <
+        Name > { props.char.name } < /Name> <
+        CharInfo info = { props.char }
+        /> <
+        /Card>
+    )
+}
+
+export default CharCard;
---
+++ starwars/src/components/CharInfo/CharInfo.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const InfoDiv = styled.div `
+        width: 100%;
+        border-left: 2px solid black;
+        `;
+
+const Span = styled.span `
+        font-weight:bold;
+        `;
+
+const CharInfo = (props) => {
+    console.log(props)
+    return ( <
+        InfoDiv >
+        <
+        p > < Span > Birth - Year: < /Span> {props.info.birth_year}</p >
+        <
+        p > < Span > Gender: < /Span> {props.info.gender}</p >
+        <
+        p > < Span > Height: < /Span> {props.info.height}</p >
+        <
+        p > < Span > Weight: < /Span> {props.info.mass}</p >
+        <
+        /InfoDiv>
+    )
+};
+
+export default CharInfo;
