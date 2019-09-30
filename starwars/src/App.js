dd72e459672b5b7b87deaf28e0f14d2902ab6bc5
Brian Moir <brianmoir@Brians-MacBook-Pro.local>
2019-09-29 23:38:16 -0400

    ?

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -8,13 +8,17 @@ import CharCard from './components/CharCard/CharCard';
 const App = () => {
         // Try to think through what state you'll need for this app before starting. Then build out
         // the state properties here.
-        const [chars, setChars] = useState();
+         const [chars, setChars] = useState();
         // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
         // side effect in a component, you want to think about which state and/or props it should
         // sync up with, if any.
+       
+
         useEffect(() => {
-            axios.get('https://swapi.co/api/people/1/')
+            axios.get('https://swapi.co/api/people/?page=2')
                 .then(response => {
+
+                    console.log(response)
                     setChars(response.data.results)
                 })
                 .catch(error => console.log(error));
